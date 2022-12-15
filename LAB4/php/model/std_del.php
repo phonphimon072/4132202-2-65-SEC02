75787a276335a293a594c5e04206dd2eed565cc1
phonphimon072 <620112418072@bru.ac.th>
2022-12-15 11:28:14 +0700

    LAB4

---
+++ LAB4/php/model/std_del.php
@@ -0,0 +1,14 @@
+<?php
+$id = $_GET['id'];
+
+require "condb.php";
+
+try {
+    $sql = "DELETE FROM td_student WHERE std_id = '$id' ";
+    mysqli_query($link,$sql);
+    echo "Affected : " . mysqli_affected_rows($link);
+} catch (Exception $e){
+    echo $e. "Error no : " . mysqli_errno($link);
+}
+
+?>
